ddc72d89dc1a6599efcd76ace1f53b0d4784ff82
egpython19 <53989681+egpython19@users.noreply.github.com>
2020-05-05 16:47:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -9,20 +8,30 @@ app = Flask(__name__)
 
 def get_fact():
 
-    response = requests.get("http://unkno.com")
-
+    url = "http://unkno.com"
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
     return facts[0].getText()
 
 
+def piggy_words(fact):
+
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    payload = {'input_text': fact}
+    r = requests.post(url, payload, allow_redirects=False)
+
+    return r.headers['Location']
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+
+    fact = get_fact()
+    return piggy_words(fact)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
